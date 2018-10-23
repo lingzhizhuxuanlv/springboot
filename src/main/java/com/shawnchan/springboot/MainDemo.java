cd28ffb249732d1d79b76b5d686369dfc528607c
Shawn Chan <970554861@qq.com>
2018-10-23 15:08:26 +0800

    HttpUtil

--- src/main/java/com/shawnchan/springboot/MainDemo.java
+++ src/main/java/com/shawnchan/springboot/MainDemo.java
@@ -1,8 +1,11 @@
 package com.shawnchan.springboot;
 
+import cn.hutool.http.HttpUtil;
+
 public class MainDemo {
 
     public static void main(String[] args) throws Exception{
+        HttpUtil.createPost("http://localhost:8080/tyjws-mini/api/member/message");
         System.out.println("");
     }
 
